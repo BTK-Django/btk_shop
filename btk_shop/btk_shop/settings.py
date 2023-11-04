ff60a1481ca1e263ad787355a1fdc878c6a2739c
Sinan URUN <sinanurun24@gmail.com>
2023-11-04 15:58:00 +0300

    yönetim arayüz dili dğeişti

--- btk_shop/btk_shop/settings.py
+++ btk_shop/btk_shop/settings.py
@@ -107,8 +107,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
+# LANGUAGE_CODE = 'en-us'
 LANGUAGE_CODE = 'tr'
-
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
