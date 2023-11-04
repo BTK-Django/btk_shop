7300f7466262966835323a9229f7fc90b1b9c915
Sinan URUN <sinanurun24@gmail.com>
2023-11-04 14:48:07 +0300

    product uygulaması oluşturuldu

--- btk_shop/home/urls.py
+++ btk_shop/home/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     # ex: /polls/
+
     path("", views.index, name="index"),
     # ex: /polls/5/
     # path("<int:question_id>/", views.detail, name="detail"),
