69a77213e2ffde6e3facc7fea8edb46fce08406d
Sinan URUN <sinanurun24@gmail.com>
2023-11-04 15:05:39 +0300

    migrate ederek hazır tabloları oluşturduk

--- btk_shop/product/models.py
+++ btk_shop/product/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
-# Create your models here.
+class Category(models.Model):
+    STATUS = ( ('True', 'Evet'),('False', 'Hayir') )
+    title = models.CharField(max_length=30)
+    keywords = models.CharField(max_length=250)
+    description = models.CharField(max_length=250)
+    image = models.ImageField(blank=True, upload_to='images/')
+    status = models.CharField(max_length=10, choices=STATUS)
+    slug = models.SlugField()
+    parent = models.ForeignKey('self', blank=True, null= True, related_name='children',
+                               on_delete=models.CASCADE)
+    create_at = models.DateTimeField(auto_now=True)
+    update_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.title
