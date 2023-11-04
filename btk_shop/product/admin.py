e51535b7345308a956cdcc4f5c0367f7a5c5924c
Sinan URUN <sinanurun24@gmail.com>
2023-11-04 16:58:22 +0300

    yeni ürün ekleme ve adminde gerekli ayarların yapılması

--- btk_shop/product/admin.py
+++ btk_shop/product/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
-from product.models import Category
+from product.models import Category, Product
+
 
 # Register your models here.
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title', 'status']
     list_filter = ['status']
     # fields = ['status']
-
-
 admin.site.register(Category, CategoryAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    # fields = ['title', 'status']
+    list_display = ['title', 'category_tag']
+    list_filter = ['status','category']
+admin.site.register(Product, ProductAdmin)
--- btk_shop/product/models.py
+++ btk_shop/product/models.py
@@ -29,5 +29,10 @@ class Product(models.Model):
     status=models.CharField(max_length=10,choices=STATUS)
     create_at=models.DateTimeField(auto_now_add=True)
     update_at=models.DateTimeField(auto_now=True)
+
+    def category_tag(self):
+        return self.category
+    category_tag.short_description = "Kategori"
+
     def __str__(self):
         return self.title
