401a35b9994e56072f935f000ebd6d33bca9db57
Sinan URUN <sinanurun24@gmail.com>
2023-11-04 16:26:51 +0300

    product modeli

--- btk_shop/product/admin.py
+++ btk_shop/product/admin.py
@@ -5,6 +5,7 @@ from product.models import Category
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title', 'status']
     list_filter = ['status']
+    # fields = ['status']
 
 
 admin.site.register(Category, CategoryAdmin)
--- btk_shop/product/models.py
+++ btk_shop/product/models.py
@@ -13,3 +13,21 @@ class Category(models.Model):
     update_at = models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.title
+class Product(models.Model):
+    STATUS = (('True', 'Evet'),
+        ('False', 'Hayir'), )
+    category = models.ForeignKey(Category, on_delete=models.CASCADE) #many to one relation with Category
+    title = models.CharField(max_length=150)
+    keywords = models.CharField(max_length=255)
+    description = models.TextField(max_length=255)
+    image=models.ImageField(blank=True, upload_to='images/')
+    # price = models.DecimalField(max_digits=12, decimal_places=2,default=0)
+    price= models.FloatField()
+    amount=models.IntegerField(default=0)
+    # detail=RichTextUploadingField()
+    detail = models.TextField()
+    status=models.CharField(max_length=10,choices=STATUS)
+    create_at=models.DateTimeField(auto_now_add=True)
+    update_at=models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.title
