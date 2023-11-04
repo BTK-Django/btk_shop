e8b797d026d69b6cbf2f03535043caf08851b93e
Sinan URUN <sinanurun24@gmail.com>
2023-11-04 16:15:27 +0300

    category adminde nasıl görüntülenecek

--- btk_shop/product/admin.py
+++ btk_shop/product/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from product.models import Category
 
 # Register your models here.
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status']
+    list_filter = ['status']
+
+
+admin.site.register(Category, CategoryAdmin)
