0b46c52df5e8f784ee06d24874e7ceadca5ff7fe
Sinan URUN <sinanurun24@gmail.com>
2023-11-12 14:36:50 +0300

    setting modeli yuklendi

--- btk_shop/home/models.py
+++ btk_shop/home/models.py
@@ -26,6 +26,5 @@ class Setting(models.Model):
     status = models.CharField(max_length=10, choices=STATUS)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
-
     def __str__(self):
         return self.title
