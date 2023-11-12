ef5fed62de917c4299a15e26e718876bb8a62e98
Sinan URUN <sinanurun24@gmail.com>
2023-11-12 14:07:58 +0300

    adminde ve product bolumunde resim goruntuleme ve ekleme islemleri yapıldı

--- btk_shop/product/admin.py
+++ btk_shop/product/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from product.models import Category, Product, Images
 
-
+class ProductImagesInline(admin.TabularInline):
+    model = Images
+    extra = 5
+    readonly_fields = ('image_tag',)
 # Register your models here.
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title', 'status']
@@ -9,14 +12,17 @@ class CategoryAdmin(admin.ModelAdmin):
     # fields = ['status']
 admin.site.register(Category, CategoryAdmin)
 
+
+
 class ProductAdmin(admin.ModelAdmin):
     # fields = ['title', 'status']
-    list_display = ['title', 'category_tag']
+    list_display = ['title', 'category_tag','image_tag']
     list_filter = ['status','category']
+    readonly_fields = ('image_tag',)
+    inlines = [ProductImagesInline]
 admin.site.register(Product, ProductAdmin)
-
 
 class ImagesAdmin(admin.ModelAdmin):
     # fields = ['title', 'status']
-    list_display = ['title','product', 'image']
+    list_display = ['title','product', 'image_tag']
 admin.site.register(Images, ImagesAdmin)
--- btk_shop/product/models.py
+++ btk_shop/product/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.utils.safestring import mark_safe
+
+
 class Category(models.Model):
     STATUS = ( ('True', 'Evet'),('False', 'Hayir') )
     title = models.CharField(max_length=30)
@@ -36,6 +39,10 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+
+    image_tag.short_description = 'Image'
 
 class Images(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
@@ -44,3 +51,6 @@ class Images(models.Model):
 
     def __str__(self):
         return self.title
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+    image_tag.short_description = 'Image'
