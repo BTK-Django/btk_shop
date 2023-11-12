fb06e5a4b5f4d5747b79538d185296b65714fa9f
Sinan URUN <sinanurun24@gmail.com>
2023-11-12 10:58:11 +0300

    stat'c '.'ne alinmi; hal'

--- btk_shop/btk_shop/urls.py
+++ btk_shop/btk_shop/urls.py
@@ -16,10 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls.static import static
+from btk_shop import settings
 urlpatterns = [
     path('', include('home.urls')),
     path('home/', include('home.urls')),
     path('admin/', admin.site.urls),
     path('product/', include('product.urls')),
 ]
+
+# resimlerin veya static dosyaların admin tarafında gösterilmesi için
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
